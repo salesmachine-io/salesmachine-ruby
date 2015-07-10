f264601a45f29501b6200082030e283b1be1a18e
Lucas Bonnet <lucas.lbonnet@gmail.com>
2015-07-10 17:03:00 +0200

    Changed parameters names

--- lib/salesmachine-ruby/email.rb
+++ lib/salesmachine-ruby/email.rb
@@ -1,10 +1,10 @@
 module SalesMachine
 
-	class Email
-		def self.send(unique_id,email_name,data={})
-			message = {  :unique_id=>unique_id.to_s,:email_name=>email_name,:params=>data}
-			publisher=Publisher.new()
-			return 	publisher.post(:email, message)
-		end
-	end
+  class Email
+    def self.send(email_uid, email_name, data={})
+      message = { :email_uid => email_uid.to_s, :email_name => email_name, :params => data }
+      publisher = Publisher.new()
+      return  publisher.post(:email, message)
+    end
+  end
 end
