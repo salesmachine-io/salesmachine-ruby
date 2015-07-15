d393946aadafbdcdda927ec767b0e8b2bafd7d35
Lucas Bonnet <lucas.lbonnet@gmail.com>
2015-07-15 11:18:24 +0200

    Fixed indentation + email contact_uid

--- lib/salesmachine-ruby/contact.rb
+++ lib/salesmachine-ruby/contact.rb
@@ -1,9 +1,9 @@
 module SalesMachine
 
-	# Alias of USER will be deprecated
-	class Contact
-		def self.set(contact_uid, data={})
-			User.set(contact_uid, data)
-		end
-	end
+  # Alias of USER will be deprecated
+  class Contact
+    def self.set(contact_uid, data={})
+      User.set(contact_uid, data)
+    end
+  end
 end
